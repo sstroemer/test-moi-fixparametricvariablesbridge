9282240cd55cfc7f9f3bc1aa9104e04087c8de61
sstroemer <stefan.stroemer@gmail.com>
2023-02-12 22:58:05 +0100

    Updates

--- verify_5.jl
+++ verify_5.jl
@@ -6,13 +6,15 @@ enable_parameters!(model)
 
 @variable(model, x[1:3] >= 0)
 @parameter(model, p[1:2], [1.0, 1.0]; fix=false)
+@parameter(model, q, 5.0)
 
-@pconstraint(model, c1, (p[1] + p[2]) * x[1] + 2*p[2]*x[2] + 2*x[1] + x[3] >= 5)
+@pconstraint(model, c, (p[1] + p[2]) * x[1] + 2*p[2]*x[2] + 2*x[1] + x[3] >= q)
 
 @objective(model, Min, sum(x))
 
 optimize!(model)
-print(c1)
+print(c)
+reduced_cost(q)
 
 set_value(p[2], 0.0; fix=false)
 # model.ext[:__parameters][p[2]].value = 0.0
